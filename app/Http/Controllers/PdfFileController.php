b1795c4b91c6a74039a2be59ce5e877344091b16
irfnhanif <14.irfanh@gmail.com>
2026-02-05 15:37:59 +0700

    Add PdfFileController with CRUD methods and update API routes

---
+++ app/Http/Controllers/PdfFileController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PdfFileController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function generate(Request $request)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\PdfFileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('pdf')->name('pdf.')->group(function () {
+    Route::post('/generate', [PdfFileController::class, 'generate'])->name('generate');
+    Route::post('/upload', [PdfFileController::class, 'store'])->name('store');
+    Route::get('/list', [PdfFileController::class, 'index'])->name('index');
+    Route::delete('/{id}', [PdfFileController::class, 'destroy'])->name('destroy');
+});
